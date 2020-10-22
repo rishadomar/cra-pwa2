72a98ce98b976ad9dd383820eed06ba12ca4b439
Rishad Omar <rishad@aux.co.za>
2020-10-22 12:49:46 +0200

    Fix warning

--- src/utilities.js
+++ src/utilities.js
@@ -1,7 +1,7 @@
 export function urlBase64ToUint8Array(base64String) {
     var padding = '='.repeat((4 - base64String.length % 4) % 4);
     var base64 = (base64String + padding)
-        .replace(/\-/g, '+')
+        .replace(/-/g, '+')
         .replace(/_/g, '/');
 
     var rawData = window.atob(base64);
